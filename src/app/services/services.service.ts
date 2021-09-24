75fba3e6f6c29a8bc7a598628a1fc824f54162e3
Jair Manjarres <jhayr.m.r@gmail.com>
2021-09-24 14:00:22 -0500

    localstorage

--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
-
+import { Task } from '../models/task';
 @Injectable({
   providedIn: 'root'
 })
 export class ServicesService {
+  tasks: Task[];
+  constructor() {
+    this.tasks = [];
+  }
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////
+  getTask(): Task[] {
+    if (localStorage.getItem('tasks') == null) {
+      this.tasks = [];
+    } else {
+      this.tasks = JSON.parse(localStorage.getItem('tasks')!)
+    }
+    return this.tasks;
+  }
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+  addcita(task: Task): void {
+    this.tasks.unshift(task);
+    if (localStorage.getItem('tasks') == null) {
+      this.tasks = [];
+      this.tasks.unshift(task);
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
 
-  constructor() { }
+    } else {
+      this.tasks = JSON.parse(localStorage.getItem('tasks')!)
+      this.tasks.unshift(task);
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    }
+
+  }
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////
+  deletecita(task:Task){
+    for(let i = 0; this.tasks.length; i++){
+      if(task == this.tasks[i]){
+this.tasks.splice(i,1);
+localStorage.setItem('tasks', JSON.stringify(this.tasks));
+      }
+    }
+  }
 }

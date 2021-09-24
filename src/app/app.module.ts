c6493172e983628e13779893d8135b5003674ffd
Jair Manjarres <jhayr.m.r@gmail.com>
2021-09-24 13:00:48 -0500

    se agregaron componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddCitaComponent } from './components/add-cita/add-cita.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { ListCitaComponent } from './components/list-cita/list-cita.component';
+import { ServicesService } from './services/services.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     AddCitaComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListCitaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [ServicesService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
